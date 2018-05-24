3ce441ee613d376670965aded4982e4e6d89646a
Kayser <zkayser@gmail.com>
2018-05-23 22:02:19 -0400

    Wire up Facebook single sign on

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PokerEx' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PokerEx
+  pod 'FacebookCore'
+  pod 'FacebookLogin'
+
+  target 'PokerExTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PokerExUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- PokerEx/ViewController.swift
+++ PokerEx/ViewController.swift
@@ -1,25 +1,18 @@
-//
-//  ViewController.swift
-//  PokerEx
-//
-//  Created by Kayser, Zack (NonEmp) on 5/21/18.
-//  Copyright © 2018 Kayser, Zack. All rights reserved.
-//
-
 import UIKit
+import FacebookLogin
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = LoginButton(readPermissions: [.publicProfile, .email])
+        loginButton.center = view.center
+        view.addSubview(loginButton)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
